c90374633912e715151bea9aa232e7ce84ef6c77
MDGcode <adrianboscanici@yahoo.com>
2024-08-13 21:11:49 +0300

    Ceva addd

--- client/src/views/Login.tsx
+++ client/src/views/Login.tsx
@@ -1,6 +1,9 @@
+import Navbar from "../components/Navbar";
+
 export default function Login() {
   return (
     <>
+      <Navbar />
       <div>Login</div>
       <div>{import.meta.env.VITE_API_URL}</div>
     </>
